1da64034379f37eaee73cc20cd53e8c97c631a5a
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2025-03-05 21:56:24 +0530

    feat: added pagination question of frontend interview

---
+++ app/question3/page.tsx
@@ -0,0 +1,10 @@
+import Question3 from '@/components/Question3'
+import Template from '@/Template/Template'
+import React from 'react'
+
+const page = () => {
+  return (
+    <Template Heading='Pagination in Web page' solution={<Question3/>} questionNumber='Question3' />
+  )
+}
+export default page
