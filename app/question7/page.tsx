1f7434c6404d50ef0cfb6ad5c9c51ed3be035b8e
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2025-03-13 21:14:30 +0530

    fix: basic ui fixes

--- app/question7/page.tsx
+++ app/question7/page.tsx
@@ -1,10 +1,10 @@
-import Question7 from '@/components/question7'
+import Question7 from '@/components/Question7'
 import Template from '@/Template/Template'
 import React from 'react'
 
 const page = () => {
     return (
-        <Template Heading='Question' solution={<Question7 />} nextQuestion='question8' />
+        <Template Heading='Question' solution={<Question7/>} nextQuestion='question8' />
     )
 }
 
--- components/Question7.tsx
+++ components/Question7.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const Question7 = () => {
   return (
-    <div>
+    <div className='h-[70vh] bg-slate-800 w-full rounded-lg p-6 flex gap-4 '>
+
       
     </div>
   )
