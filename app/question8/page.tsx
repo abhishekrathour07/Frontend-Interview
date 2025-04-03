90873285d041c73f4778320c5ae0aeac23279fcd
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2025-04-03 22:44:18 +0530

    otp varification question

---
+++ app/question8/page.tsx
@@ -0,0 +1,11 @@
+
+import Template from '@/Template/Template'
+import React from 'react'
+
+function page() {
+  return (
+    <Template Heading='Otp varification UI' nextQuestion='question9'/>
+  )
+}
+
+export default page
