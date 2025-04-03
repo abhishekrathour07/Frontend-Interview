15680416cc8a041f590b99fd9d7035afbcc5242a
abhishekrathour07 <singhrathourabhishek7@gmail.com>
2025-04-03 22:45:41 +0530

    question8 otp varificatoion complete

--- app/question8/page.tsx
+++ app/question8/page.tsx
@@ -1,10 +1,11 @@
 
+import Question8 from '@/components/Question8'
 import Template from '@/Template/Template'
 import React from 'react'
 
 function page() {
   return (
-    <Template Heading='Otp varification UI' nextQuestion='question9'/>
+    <Template Heading='Otp varification UI' solution={<Question8/>} nextQuestion='question9'/>
   )
 }
 
